4ba36749dc8d7a51dfca51d547664682471dfad7
Hongsoojeong <my_youth99@naver.com>
2022-01-21 21:04:15 +0900

    왕실나이트(피드백)

---
+++ 구현 알고리즘/왕실나이트(피드백).py
@@ -0,0 +1,13 @@
+coor=input() #좌표
+row=int(coor[1]) 
+col=int(ord(coor[0]))-int(ord('a'))+1 
+#a:1, b:2 ...
+print(col)
+steps=[(-2,-1),(-1,-2),(1,-2),(2,-1),(2,1),(1,2),(-1,2),(-2,1)]
+ct=0
+for step in steps:
+  next_row=row+step[0]
+  next_column=col+step[1]
+  if next_row >=1 and next_row<=8 and next_column>=1 and next_column<=8:
+    ct+=1
+print(ct)
